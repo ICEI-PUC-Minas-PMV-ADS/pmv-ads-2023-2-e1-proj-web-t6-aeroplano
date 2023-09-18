2baeee43ad8632948ae321d381d865a53f181fd1
José Antônio <50558024+joseantonion03@users.noreply.github.com>
2023-09-18 15:08:58 -0300

    Build: Novo template
    Template informando que o site está em andamento

---
+++ codigo-fonte/assets/javascript/index.js
@@ -0,0 +1,27 @@
+import "./main.js";
+
+const loading = document.getElementById("loadingContent");
+const image = document.getElementById("bgimage");
+
+const apiKey = "mwq8fMA8XVjkukAZNIbg8Bx9Np99bC4zSTNkKgJEgbo";
+
+const searchQuery = "aviao";
+
+async function fetchImages() {
+  try {
+    const response = await fetch(
+      `https://api.unsplash.com/photos/random?client_id=${apiKey}&query=${searchQuery}`
+    );
+    const data = await response.json();
+
+    image.src = data.urls.regular;
+    image.alt = data.alt_description;
+    setTimeout(() => {
+      loading.classList.add("hidden");
+    }, 2000);
+  } catch (error) {
+    console.error("Erro ao buscar imagens:", error);
+  }
+}
+
+window.addEventListener("load", fetchImages);
--- codigo-fonte/assets/javascript/main.js
+++ codigo-fonte/assets/javascript/main.js
@@ -1,9 +1,9 @@
 tailwind.config = {
-    theme: {
-      extend: {
-        colors: {
-          clifford: '#da373d',
-        }
-      }
-    }
-  }
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+      },
+    },
+  },
+};
