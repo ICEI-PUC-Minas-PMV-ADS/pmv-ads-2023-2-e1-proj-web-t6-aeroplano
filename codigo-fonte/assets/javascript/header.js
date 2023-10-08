8388d94e781c311921f4bab1a66078060884bd47
RafaelMourao <mourao.ea@gmail.com>
2023-10-08 17:38:28 -0300

    feature: Adiciona header como arquivo separado

--- codigo-fonte/assets/javascript/header.js
+++ codigo-fonte/assets/javascript/header.js
@@ -1,10 +1,26 @@
+function includeHeader() {
+  var headerPlaceholder = document.getElementById("header");
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "header.html", true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      headerPlaceholder.innerHTML = xhr.responseText;
+    }
+  };
+  console.log(xhr);
+  xhr.send();
+}
+
+// Chame a função para incluir o header
+includeHeader();
+
 const openMenu = document.querySelector(".open-menu");
 const closeMenu = document.querySelector(".close-menu");
 const menu = document.querySelector(".directories");
 
-openMenu.addEventListener("click", () => {
-  menu.classList.add("activeMenu");
-});
-closeMenu.addEventListener("click", () => {
-  menu.classList.remove("activeMenu");
-});
+// openMenu.addEventListener("click", () => {
+//   menu.classList.add("activeMenu");
+// });
+// closeMenu.addEventListener("click", () => {
+//   menu.classList.remove("activeMenu");
+// });
