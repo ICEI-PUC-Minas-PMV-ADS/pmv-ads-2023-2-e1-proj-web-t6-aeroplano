6d21b85f465c07dda2667ac06b35a3a52359ac8c
RafaelMourao <mourao.ea@gmail.com>
2023-10-14 08:02:01 -0300

    feature: Deixa a pagina login responsiva

--- codigo-fonte/assets/javascript/login.js
+++ codigo-fonte/assets/javascript/login.js
@@ -3,3 +3,30 @@ import "./header.js";
 import "./footer.js";
 
 // DIGITE SEU CODIGO JAVASCRIPT
+
+const signInButton = document.getElementById("signInButton");
+
+signInButton.addEventListener("click", verifyCredentials);
+
+function verifyCredentials() {
+  var emailField = document.getElementById("emailInput");
+  var email = emailField.value;
+
+  if (ValidateEmail(email)) {
+    console.log("E-mail válido!");
+  } else {
+    console.log("E-mail inválido.");
+  }
+}
+
+function ValidateEmail(mail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+    return true;
+  }
+  return false;
+}
+
+// Previne que a pagina seja recarregada depois que o botão submit (entrar)  é pressionado
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+});
