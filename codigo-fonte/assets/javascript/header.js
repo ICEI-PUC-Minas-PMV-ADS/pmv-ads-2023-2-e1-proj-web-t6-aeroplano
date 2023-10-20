bf38f0b6e79b679c416fbb5b47d3e4edb7418d07
RafaelMourao <mourao.ea@gmail.com>
2023-10-20 09:17:22 -0300

    fix: Corrige menu não abrindo

--- codigo-fonte/assets/javascript/header.js
+++ codigo-fonte/assets/javascript/header.js
@@ -2,15 +2,15 @@ const headerTemplate = document.getElementById("header");
 
 fetch("./header.html")
   .then((resp) => resp.text())
-  .then((dados) => (headerTemplate.innerHTML = dados));
-
-const openMenu = document.querySelector(".open-menu");
-const closeMenu = document.querySelector(".close-menu");
-const menu = document.querySelector(".directories");
-
-//openMenu.addEventListener("click", () => {
-//  menu.classList.add("activeMenu");
-//});
-//closeMenu.addEventListener("click", () => {
-//  menu.classList.remove("activeMenu");
-//});
+  .then((dados) => {
+    headerTemplate.innerHTML = dados;
+    const openMenu = document.querySelector(".open-menu");
+    const closeMenu = document.querySelector(".close-menu");
+    const menu = document.querySelector(".directories");
+    openMenu.addEventListener("click", () => {
+      menu.classList.add("activeMenu");
+    });
+    closeMenu.addEventListener("click", () => {
+      menu.classList.remove("activeMenu");
+    });
+  });
